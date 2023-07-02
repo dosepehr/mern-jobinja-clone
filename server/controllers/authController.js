1e4715ec9056930affdd2d9a0cb1c914e6c3abb5
dosepehr <dosepehr@gmail.com>
2023-07-02 17:05:16 +0330

    add protect to auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -30,3 +30,45 @@ exports.login = catchAsync(async (req, res, next) => {
     // if everything was ok,send the token
     createSendToken(user, 200, res);
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+    // getting token
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }
+    if (!token) {
+        return next(new ErrorHandler('you are not logged in!', 401));
+    }
+    // validating token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    // check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new ErrorHandler(
+                'the user belonging to this token does no longer exist',
+                401
+            )
+        );
+    }
+    // Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new ErrorHandler(
+                'User recently changed password! Please log in again.',
+                401
+            )
+        );
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    next();
+});
+
