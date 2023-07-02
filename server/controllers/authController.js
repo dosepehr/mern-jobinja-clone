834a209431bce118170f322a5894022d6e8ee1f4
dosepehr <dosepehr@gmail.com>
2023-07-02 17:05:45 +0330

    add restrictTo auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -72,3 +72,17 @@ exports.protect = catchAsync(async (req, res, next) => {
     next();
 });
 
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new ErrorHandler(
+                    'You do not have permission to perform this action',
+                    403
+                )
+            );
+        }
+
+        next();
+    };
+};
