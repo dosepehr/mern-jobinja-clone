f45f4cb5bb07b5050ae3c3db11705ef2341d6de0
dosepehr <dosepehr@gmail.com>
2023-07-06 01:28:59 +0330

    add router

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{js,jsx}'],
+    theme: {
+        extend: {
+            colors: {
+                bgGray: '#444444',
+                textGray: '#555555',
+            },
+        },
+    },
+    plugins: [],
+};
