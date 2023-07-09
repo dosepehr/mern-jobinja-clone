9c402fae933b5f036136df31ca59acc1301c52a4
dosepehr <dosepehr@gmail.com>
2023-07-09 22:10:46 +0330

    add signup employer validation

--- client/src/validation/usersValidation.js
+++ client/src/validation/usersValidation.js
@@ -1,5 +1,4 @@
 import * as Yup from 'yup';
-// ! validation schema on registering
 export const loginUserShema = Yup.object().shape({
     password: Yup.string()
         .required('رمز عبور را وارد کنید')
@@ -17,5 +16,19 @@ export const signUpUserShema = Yup.object().shape({
     email: Yup.string()
         .email('آدرس وارد شده صحیح نمیباشد')
         .required('ایمیل را وارد کنید'),
+    confirmPassword: Yup.string().required('رمز عبور را تکرار کنید'),
+});
+
+export const signUpEmployer = Yup.object.shape({
+    username: Yup.string().required('نام و نام خانوادگی را وارد کنید'),
+    email: Yup.string()
+        .email('آدرس وارد شده صحیح نمیباشد')
+        .required('ایمیل را وارد کنید'),
+    phone: Yup.string()
+        .min(11, 'شماره موبایل معتبر نمیباشد')
+        .max(11, 'شماره موبایل معتبر نمیباشد'),
+    password: Yup.string()
+        .required('رمز عبور را وارد کنید')
+        .min(8, 'رمز عبور باید حداقل 8 کاراکتر باشد'),
     confirmPassword: Yup.string().required('رمز عبور را تکرار کنید'),
 });
