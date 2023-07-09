00bd9f9843619322b6ee450f39999667c46e9ff3
dosepehr <dosepehr@gmail.com>
2023-07-09 22:31:31 +0330

    add login employer page

--- client/src/components/LoginEmployer.jsx
+++ client/src/components/LoginEmployer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { signUpEmployer } from '../validation/usersValidation';
+import { Formik, Field, ErrorMessage, Form } from 'formik';
 const LoginEmployer = () => {
     return (
         <div className='min-h-screen bg-[#f5f5f5] pb-10'>
@@ -16,60 +17,96 @@ const LoginEmployer = () => {
                 <p className='text-mainmainDarkGray text-3xl text-[#595f66] font-semibold text-center'>
                     ثبت نام کارفرمایان
                 </p>
-                <form className='bg-white shadow-xl mx-auto w-[400px] max-w-[90%] mt-10 p-8 space-y-5 rounded-lg'>
-                    <div className='flex flex-col justify-start'>
-                        <p className='mb-3 text-mainLightGray'>
-                            نام و نام خانوادگی
-                        </p>
-                        <input
-                            type='text'
-                            name=''
-                            id=''
-                            placeholder='مثلا: علی محمدی'
-                            className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
-                        />
-                    </div>
-                    <div className='flex flex-col justify-start'>
-                        <p className='mb-3 text-mainLightGray'>ایمیل</p>
-                        <input
-                            type='email'
-                            placeholder='example@gmail.com'
-                            className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
-                        />
-                    </div>
-                    <div className='flex flex-col justify-start'>
-                        <p className='mb-3 text-mainLightGray'>شماره موبایل</p>
-                        <input
-                            type='number'
-                            placeholder='09123456789'
-                            className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
-                        />
-                    </div>
-                    <div className='flex flex-col justify-start'>
-                        <p className='mb-3 text-mainLightGray'>
-                            انتخاب رمز عبور
-                        </p>
-                        <input
-                            type='password'
-                            placeholder='حداقل 8 کاراکتر'
-                            className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
-                        />
-                    </div>
-                    <div className='flex flex-col justify-start'>
-                        <p className='mb-3 text-mainLightGray'>
-                            تکرار رمز عبور
-                        </p>
-                        <input
-                            type='text'
-                            name=''
-                            id=''
-                            className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
-                        />
-                    </div>
-                    <button className='w-full py-2 bg-mainDarkBlue text-white rounded'>
-                        ثبت نام
-                    </button>
-                </form>
+                <Formik
+                    initialValues={{
+                        username: '',
+                        email: '',
+                        phone: '',
+                        password: '',
+                        confirmPassword: '',
+                    }}
+                    validationSchema={signUpEmployer}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    <Form className='bg-white shadow-xl mx-auto w-[400px] max-w-[90%] mt-10 p-8 space-y-5 rounded-lg'>
+                        <div className='flex flex-col justify-start'>
+                            <p className='mb-3 text-mainLightGray'>
+                                نام و نام خانوادگی
+                            </p>
+                            <Field
+                                type='text'
+                                name='username'
+                                id=''
+                                placeholder='مثلا: علی محمدی'
+                                className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
+                            />
+                            <span className='text-red-600 text-sm'>
+                                <ErrorMessage name='username' />
+                            </span>
+                        </div>
+                        <div className='flex flex-col justify-start'>
+                            <p className='mb-3 text-mainLightGray'>ایمیل</p>
+                            <Field
+                                name='email'
+                                type='email'
+                                placeholder='example@gmail.com'
+                                className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
+                            />
+                            <span className='text-red-600 text-sm'>
+                                <ErrorMessage name='email' />
+                            </span>
+                        </div>
+                        <div className='flex flex-col justify-start'>
+                            <p className='mb-3 text-mainLightGray'>
+                                شماره موبایل
+                            </p>
+                            <Field
+                                name='phone'
+                                type='text'
+                                placeholder='09123456789'
+                                className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
+                            />
+                            <span className='text-red-600 text-sm'>
+                                <ErrorMessage name='phone' />
+                            </span>
+                        </div>
+                        <div className='flex flex-col justify-start'>
+                            <p className='mb-3 text-mainLightGray'>
+                                انتخاب رمز عبور
+                            </p>
+                            <Field
+                                name='password'
+                                type='password'
+                                placeholder='حداقل 8 کاراکتر'
+                                className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
+                            />
+                            <span className='text-red-600 text-sm'>
+                                <ErrorMessage name='password' />
+                            </span>
+                        </div>
+                        <div className='flex flex-col justify-start'>
+                            <p className='mb-3 text-mainLightGray'>
+                                تکرار رمز عبور
+                            </p>
+                            <Field
+                                name='confirmPassword'
+                                type='password'
+                                className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
+                            />
+                            <span className='text-red-600 text-sm'>
+                                <ErrorMessage name='confirmPassword' />
+                            </span>
+                        </div>
+                        <button
+                            type='submit'
+                            className='w-full py-2 bg-mainDarkBlue text-white rounded'
+                        >
+                            ثبت نام
+                        </button>
+                    </Form>
+                </Formik>
             </div>
         </div>
     );
