b8211b5ef43d9dbc5325b2b03d9b0ca7e0f4326d
dosepehr <dosepehr@gmail.com>
2023-07-10 21:41:12 +0330

    add signup user action

--- client/src/redux/reducers/authSlice.js
+++ client/src/redux/reducers/authSlice.js
@@ -1,10 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+export const signupUser = createAsyncThunk('/users/signup', async (body) => {
+    const { data } = await axios.post(
+        'http://localhost:5000/api/v1/users/signup',
+        body
+    );
+    return data;
+});
 
 const authSlice = createSlice({
     name: 'auth',
     initialState: {
         token: '',
         user: [],
+    },
+    extraReducers: (builder) => {
+        builder.addCase(signupUser.fulfilled, (state, action) => {
+            state.token = action.payload.token;
+            localStorage.setItem('token', action.payload.token);
+        });
     },
 });
 
