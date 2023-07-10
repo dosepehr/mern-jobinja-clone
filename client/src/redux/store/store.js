09d8fcef9bc9812ea3380ea3b86b03c3e961a7fc
dosepehr <dosepehr@gmail.com>
2023-07-10 21:24:29 +0330

    add auth slice

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../reducers/apiSlice';
+import authSlice from '../reducers/authSlice';
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authSlice,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
