b20c1adccecb7f7b2f97f87e7cebc4b13dd491c7
dosepehr <dosepehr@gmail.com>
2023-07-10 20:56:49 +0330

    add login employer form

--- client/src/components/LoginEmployer.jsx
+++ client/src/components/LoginEmployer.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-
+import { Form, Formik, ErrorMessage, Field } from 'formik';
+import { loginUserShema } from '../validation/usersValidation';
 const LoginEmployer = () => {
     return (
         <div className='min-h-screen bg-[#f5f5f5] pb-10'>
@@ -15,6 +16,54 @@ const LoginEmployer = () => {
                 </div>
                 <p className='text-2xl'>JOBINJA</p>
             </nav>
+            <div className=' mt-10'>
+                <p className='text-mainmainDarkGray text-3xl text-[#595f66] font-semibold text-center'>
+                    ورود به حساب کارفرمایی
+                </p>
+                <Formik
+                    initialValues={{
+                        email: '',
+                        password: '',
+                    }}
+                    validationSchema={loginUserShema}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    <Form className='bg-white shadow-xl mx-auto w-[400px] max-w-[90%] mt-10 p-8 space-y-5 rounded-lg'>
+                        <div className='flex flex-col justify-start'>
+                            <p className='mb-3 text-mainLightGray'>ایمیل</p>
+                            <Field
+                                name='email'
+                                type='email'
+                                placeholder='example@gmail.com'
+                                className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
+                            />
+                            <span className='text-red-600 text-sm'>
+                                <ErrorMessage name='email' />
+                            </span>
+                        </div>
+                        <div className='flex flex-col justify-start'>
+                            <p className='mb-3 text-mainLightGray'>رمز عبور</p>
+                            <Field
+                                name='password'
+                                type='password'
+                                placeholder='حداقل 8 کاراکتر'
+                                className='border border-[#e5e5e5] px-3 py-2 rounded-sm'
+                            />
+                            <span className='text-red-600 text-sm'>
+                                <ErrorMessage name='password' />
+                            </span>
+                        </div>
+                        <button
+                            type='submit'
+                            className='w-full py-2 bg-mainDarkBlue text-white rounded'
+                        >
+                            وارد شوید
+                        </button>
+                    </Form>
+                </Formik>
+            </div>
         </div>
     );
 };
