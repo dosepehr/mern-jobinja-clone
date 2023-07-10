b007e808b8997d25d7eb39ee2cbacd58a37f7fb0
dosepehr <dosepehr@gmail.com>
2023-07-10 21:50:04 +0330

    add getMe route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,18 @@
 const AppliedOffer = require('../models/AppliedOfferModel');
+const User = require('../models/UserModel');
 
-const { createOne } = require('./handlerFactory');
+const { createOne, getOne } = require('./handlerFactory');
 
 exports.applyOffer = (req, res, next) => {
     if (!req.body.offer) req.body.offer = req.params.offerId;
     if (!req.body.user) req.body.user = req.user.id;
     next();
 };
+
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id;
+    next();
+};
+exports.getUser = getOne(User);
+
 exports.addOffer = createOne(AppliedOffer);
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const { signup, login, protect } = require('../controllers/authController');
-const { applyOffer, addOffer } = require('../controllers/userController');
+const {
+    applyOffer,
+    addOffer,
+    getMe,
+    getUser,
+} = require('../controllers/userController');
 
 const userRoute = express.Router();
 
 userRoute.route('/signup').post(signup);
 userRoute.route('/login').post(login);
 userRoute.route('/addOffer/:offerId').post(protect, applyOffer, addOffer);
-
+userRoute.get('/me', protect, getMe, getUser);
 module.exports = userRoute;
